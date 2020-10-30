454f89129dacf3adf1afba3ef22ef4cdd2c2eb43
Andrew S Aguiar <andrewaguiar6@gmail.com>
2020-10-30 13:32:19 -0300

    adds new post

---
+++ 2020-10-30-starting-localhost-server-from-folder.md
@@ -0,0 +1,23 @@
+---
+layout: post
+title: "Starting localhost server from folder"
+image: "clock"
+---
+
+Sometimes we want to start a server locally to serve a local folder with some assets (html, css, js) using,
+using ruby it is easy, just run a:
+
+```
+ruby -run -ehttpd . -p8000
+```
+
+and it is done, you can access your local files using a `http://localhost:8000`.
+
+If you want to make it accessible through internet you can use the [ngrok.com](https://ngrok.com) service.
+So just make the combination of
+
+```
+ruby -run -ehttpd . -p8000 & ngrok http 8000
+```
+
+This way you will serve current folder in localhost:8000 and ngrok will make a tunnel through internet.
---
+++ new-post.rb
@@ -0,0 +1,22 @@
+require 'date'
+
+BAD_CHARS = 'ÀÁÂÃÄÅàáâãäåĀāĂăĄąÇçĆćĈĉĊċČčÐðĎďĐđÈÉÊËèéêëĒēĔĕĖėĘęĚěĜĝĞğĠġĢģĤĥĦħÌÍÎÏìíîïĨĩĪīĬĭĮįİıĴĵĶķĸĹĺĻļĽľĿŀŁłÑñŃńŅņŇňŉŊŋÒÓÔÕÖØòóôõöøŌōŎŏŐőŔŕŖŗŘřŚśŜŝŞşŠšſŢţŤťŦŧÙÚÛÜùúûüŨũŪūŬŭŮůŰűŲųŴŵÝýÿŶŷŸŹźŻżŽž'
+GOOD_CHARS = 'AAAAAAaaaaaaAaAaAaCcCcCcCcCcDdDdDdEEEEeeeeEeEeEeEeEeGgGgGgGgHhHhIIIIiiiiIiIiIiIiIiJjKkkLlLlLlLlLlNnNnNnNnnNnOOOOOOooooooOoOoOoRrRrRrSsSsSsSssTtTtTtUUUUuuuuUuUuUuUuUuUuWwYyyYyYZzZzZz'
+
+def self.parameterize(s)
+  s.strip.downcase.tr(BAD_CHARS, GOOD_CHARS).gsub(' ', '-')
+end
+
+d = Date.today
+
+content = <<-CONTENT
+---
+layout: post
+title: "#{ARGV[0]}"
+image: "closed"
+---
+
+
+CONTENT
+
+File.write("#{d}-#{parameterize(ARGV[0])}.md", content)
